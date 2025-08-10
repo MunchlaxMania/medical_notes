df0d12d136489bfd59a5b15b63110ba9120a5087
Munchlax <121154342+MunchlaxMania@users.noreply.github.com>
2025-08-10 23:04:57 +0900

    Delete src/main/resources/templates/MedicineRepository.java

--- src/main/resources/templates/MedicineRepository.java
+++
@@ -1,8 +0,0 @@
-// 薬のデータを操作するためのリポジトリ
-
-package com.example.medicineNote;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicineRepository extends JpaRepository<Medicine, Integer> {
-}
